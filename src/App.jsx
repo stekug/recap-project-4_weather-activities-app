b76924d908a27f603eb8e40417d7b019d44438a4
Stephan Kugler (WSL) <stephankugler.stuff@gmail.com>
2024-06-28 10:10:22 +0200

    initial setup vite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>recap-project-4_weather-activities-app</h1>
+      <button>test</button>
+    </>
+  );
+}
+
+export default App;
