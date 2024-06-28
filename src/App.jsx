e421530cef58be473869e685df2f3728162ac0ff
Stephan Kugler (WSL) <stephankugler.stuff@gmail.com>
2024-06-28 12:12:09 +0200

    add handleAddActivity function to app.jsx, add uid

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { useState } from 'react';
+import { uid } from 'uid';
 import './App.css';
 import Form from './components/Form';
 
 function App() {
+  const [activities, setActivities] = useState([]);
+
+  function handleAddActivity(newActivity) {
+    // Rename & Destructure
+    const { activityInput: name, isGoodWeatherCheck: isChecked } = newActivity;
+    setActivities({ id: uid(), name, isChecked });
+  }
+  console.log(activities);
+
   return (
     <>
-      <Form />
+      <Form onAddActivity={handleAddActivity} />
     </>
   );
 }
