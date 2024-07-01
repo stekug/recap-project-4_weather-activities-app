25a9e36144f18bfb042c9ed6854bb549e872ecc0
Gunnar Montag <gunnar.montag@gmx.de>
2024-07-01 17:04:35 +0200

    Update src/components/ListElement.jsx
    Co-authored-by: Merle Makoben <merlemakoben@gmail.com>

--- src/components/ListElement.jsx
+++ src/components/ListElement.jsx
@@ -4,7 +4,7 @@ export default function ListElement({ activity, onDelete }) {
   return (
     <li className="ListElement">
       {activity.name}
-      <button onClick={() => onDelete(activity.id)} className="ListElement__button">
+      <button onClick={() => onDelete(activity.id)} className="ListElement__button" aria-label="delete activity">
         x
       </button>
     </li>
